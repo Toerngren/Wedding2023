6523c8471b31ffabff67e8edcedfb87085f9f24c
A.Törngren <anna.j.torngren@gmail.com>
2023-09-15 19:27:03 +0200

    add first draft

---
+++ text.js
@@ -0,0 +1,14 @@
+const texts = {
+    schedule: {
+        title: "Bröllopsprogram",
+        content: "Här kommer information om bröllopets schema. Exempel: Ceremoni kl. 14:00, Middag kl. 18:00, Dans kl. 20:00."
+    },
+    rsvp: {
+        title: "OSA",
+        content: "Vänligen meddela oss om du kan komma senast den 1 januari. Använd formuläret nedan för att OSA."
+    },
+    accommodations: {
+        title: "Boende",
+        content: "Information om hotell och logi kommer här. Exempel: Hotell X ligger 10 minuter från festlokalen och erbjuder rabatterade priser för bröllopsgäster."
+    }
+};
